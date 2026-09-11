7b25dc920f9f90470c8c7c85557b4ed01e111334
darshanhegde204 <darshanbca204@gmail.com>
2026-09-11 15:39:39 +0530

    07 commited

---
+++ 09.py
@@ -0,0 +1,17 @@
+num = int(input("Enter a number from 1-7 "))
+if num==1:
+    print("Mon")
+elif num==2:
+    print("Tue")
+elif num==3:
+    print("Wen")
+elif num==4:
+    print("Thu")
+elif num==5:
+    print("Fri")
+elif num==6:
+    print("Sat")
+elif num==7:
+    print("Sun")
+else:
+    print("Invalid Number")
